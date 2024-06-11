a28ae95369d50ae0ab95afebf37662ffdee857fb
Evgenia Ermakova <mikassik@yandex.ru>
2024-06-11 21:00:42 +0300

    finaleWork

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -6,10 +6,10 @@ export * from './burger-ingredients';
 export * from './feed-info';
 export * from './ingredient-details';
 export * from './ingredients-category';
-export * from './modal';
-export * from './modal-overlay';
+export * from './modal'; // 1 проверить
+export * from './modal-overlay'; // 2 проверить
 export * from './order-card';
-export * from './order-details';
+export * from './order-details'; //3 проверить
 export * from './order-info';
 export * from './order-status';
 export * from './orders-list';
