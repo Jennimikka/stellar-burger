67e531403ac4b3929a18cfe565503ae04abfdc99
Evgenia Ermakova <mikassik@yandex.ru>
2024-06-11 21:33:13 +0300

    fineWork

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,15 +1,15 @@
 export * from './app-header';
 export * from './burger-constructor';
 export * from './burger-constructor-element';
-export * from './burger-ingredient';
-export * from './burger-ingredients';
+export * from './burger-ingredient'; 
+export * from './burger-ingredients'; //проверить потом
 export * from './feed-info';
 export * from './ingredient-details';
 export * from './ingredients-category';
-export * from './modal'; // 1 проверить
-export * from './modal-overlay'; // 2 проверить
+export * from './modal'; //подумать еще над реализацией
+export * from './modal-overlay'; 
 export * from './order-card';
-export * from './order-details'; //3 проверить
+export * from './order-details'; 
 export * from './order-info';
 export * from './order-status';
 export * from './orders-list';
